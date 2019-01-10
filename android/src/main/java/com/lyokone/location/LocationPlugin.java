ee31306a4b70d8d066fedc65e36a79cc8120521d
Cameron Webb <cwebb02@gmail.com>
2019-01-10 15:49:42 -0600

    adjusting code

--- android/src/main/java/com/lyokone/location/LocationPlugin.java
+++ android/src/main/java/com/lyokone/location/LocationPlugin.java
@@ -2,20 +2,13 @@ package com.lyokone.location;
 
 import android.Manifest;
 import android.app.Activity;
-import android.content.Context;
-import android.content.BroadcastReceiver;
 import android.content.IntentSender;
-import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Build;
-import android.os.Bundle;
 import android.os.Looper;
-import android.support.annotation.MainThread;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import android.util.Log;
 
 import com.google.android.gms.common.api.ApiException;
